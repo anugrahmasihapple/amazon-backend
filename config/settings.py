7dd060508078f6509264931489c6be712b4f0ae1
anugrahmasihapp <99712115+anugrahmasihapple@users.noreply.github.com>
2022-10-03 18:19:39 +0000

    Added deployed link

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 X_FRAME_OPTIONS = '*'
 CORS_ALLOW_ALL_ORIGINS = True
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://0.0.0:3000']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','http://0.0.0:3000', 'https://amazon-backend.anugrahmasihapp.repl.co']
 
 # Application definition
 
